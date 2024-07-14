be4e04877e4c2f0754d09dfbe8bedbef2c6280d3
ChandaniSahu <140198472+ChandaniSahu@users.noreply.github.com>
2024-07-14 11:43:40 -0700

    Update server.js : port is derived from process.env

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const app = express()
 const connectMongo = require('./db')
 const router = require('./Router/router')
 const cors = require('cors')
-
+const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 app.use('/api',router)
 connectMongo()
 
-app.listen(3100,()=>{
- console.log('server is stening on port 3100')
+app.listen(port,()=>{
+ console.log('server is stening on port : ',port)
 })
